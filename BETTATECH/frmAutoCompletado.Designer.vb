c968e73725a95ca6e3374150be4046c3f3c7d0be
akumanomi1988 <akumanomi1988@gmail.com>
2020-11-10 15:54:31 +0100

    First commit

---
+++ BETTATECH/frmAutoCompletado.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmAutoCompletado
+	Inherits System.Windows.Forms.Form
+
+	'Form reemplaza a Dispose para limpiar la lista de componentes.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Requerido por el Diseñador de Windows Forms
+	Private components As System.ComponentModel.IContainer
+
+	'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+	'Se puede modificar usando el Diseñador de Windows Forms.  
+	'No lo modifique con el editor de código.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.ListBox1 = New System.Windows.Forms.ListBox()
+		Me.SuspendLayout()
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(12, 28)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(218, 20)
+		Me.TextBox1.TabIndex = 0
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(13, 8)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(101, 13)
+		Me.Label1.TabIndex = 2
+		Me.Label1.Text = "Comienza a escribir:"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(14, 55)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(131, 13)
+		Me.Label2.TabIndex = 3
+		Me.Label2.Text = "Opciones autocompletado"
+		'
+		'ListBox1
+		'
+		Me.ListBox1.FormattingEnabled = True
+		Me.ListBox1.Location = New System.Drawing.Point(12, 71)
+		Me.ListBox1.Name = "ListBox1"
+		Me.ListBox1.Size = New System.Drawing.Size(218, 355)
+		Me.ListBox1.TabIndex = 4
+		'
+		'frmAutoCompletado
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(242, 450)
+		Me.Controls.Add(Me.ListBox1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Name = "frmAutoCompletado"
+		Me.Text = "frmAutoCompletado"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents TextBox1 As Windows.Forms.TextBox
+	Friend WithEvents Label1 As Windows.Forms.Label
+	Friend WithEvents Label2 As Windows.Forms.Label
+	Friend WithEvents ListBox1 As Windows.Forms.ListBox
+End Class
---
+++ BETTATECH/frmAutoCompletado.vb
@@ -0,0 +1,36 @@
+﻿
+Imports System.IO
+Imports System.Windows.Forms
+
+Class frmAutoCompletado
+	Private _ListaPalabras As List(Of String)
+	Private Sub frmAutoCompletado_Load(sender As Object, e As EventArgs) Handles Me.Load
+		_LimpiarPalabras_PasarALista()
+	End Sub
+	Private Sub _LimpiarPalabras_PasarALista()
+		'Limpieza de caracteres molestos (Solo he limpiado los que he ido encontrando)
+		Dim _CadenaPalabras As String
+		_CadenaPalabras = My.Resources.palabras.ToString
+		_CadenaPalabras = _CadenaPalabras.Replace(Environment.NewLine, ",")
+		_CadenaPalabras = _CadenaPalabras.Replace(" ", ",")
+		_CadenaPalabras = _CadenaPalabras.Replace("`", "")
+		_CadenaPalabras = _CadenaPalabras.Replace($"\", "")
+		_ListaPalabras = _CadenaPalabras.Split(",").ToList
+		_ListaPalabras.Sort()
+	End Sub
+	Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+		Cursor = Cursors.WaitCursor
+		ListBox1.Items.Clear()
+		ListBox1.Items.AddRange(FiltrarPalabras(TextBox1.Text).ToArray)
+		ListBox1.SelectedItem = -1
+		Cursor = Cursors.Default
+	End Sub
+	Private Function FiltrarPalabras(texto As String) As List(Of String)
+		Return _ListaPalabras.FindAll(Function(x) x.StartsWith(texto))
+	End Function
+	Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+		If ListBox1.SelectedIndex <> -1 Then
+			TextBox1.Text = ListBox1.SelectedItem
+		End If
+	End Sub
+End Class
